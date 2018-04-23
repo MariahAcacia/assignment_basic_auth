e4028c6d1a2f7e9a345ed321acf64e8a7fb5c0df
Mariah Schneeberger <mariah.acacia@gmail.com>
2018-04-22 18:06:47 -0700

    added basic HTTP auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  http_basic_authenticate_with name: 'admin', password: 'password', except: [:new]
+
+private
+
+
 end
